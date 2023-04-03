c593c409e6c67ed46e4e1829c01820b2f2139c91
chenjinqiu <chenjinqiu@hualala.com>
2023-04-03 17:58:03 +0800

    渲染

--- src/index.js
+++ src/index.js
@@ -135,8 +135,12 @@ const renderer = createRenderer()
 //  obj.bar = 3 
 
 
+const vnode = {
+    type: 'h1',
+    children: 'hello',
+}
 const count = ref(1)
 effect(() => {
-    renderer.render(`<h1>${count.value}</h1>`, document.getElementById('app'))
-})
+    renderer.render(vnode, document.getElementById('app'))
+}) 
 count.value++
